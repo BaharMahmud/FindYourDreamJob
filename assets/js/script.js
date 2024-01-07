15969a55c6510f08428efbdf6d71ab04be092349
BaharMahmud <bahar.a.mahmud@gmail.com>
2024-01-07 10:40:05 -0500

    script.js file is created for job search html and fetch process has been coded.The css style sheet has been changed for the same html.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,106 +1,99 @@
-var salariesForm = document.getElementById('search-salaries-form')
+var jobsFormEl = document.querySelector('#jobs-form');
+var titleInputEl = document.querySelector('#title');
+var jobsContainerEl = document.querySelector('#jobs-container');
+var jobsSearchTerm = document.querySelector('#jobs-search-term');
+var searchBtn=document.querySelector("#search");
+var footerEl=document.querySelector(".footer");
+var nextBtnEL=document.querySelector(".waves-effect waves-light btn");
+var page=1;
+var formSubmitHandler = function (event) {
+  event.preventDefault();
 
-function getArbeitNowApi(event) {
-  event.preventDefault()
-  // Construct the URL with query parameters
-  // var apiUrl = "https://www.arbeitnow.com/api/job-board-api";
-     var apiUrl = "https://api.adzuna.com/v1/api/jobs/us/search/1?app_id=fe15ccd9&app_key=7d9de1620ebcf6cd476c7c843c768025"
-  // var queryParams = {
-  //   where: location,
-  //   what: keywords
-  //   // Add more query parameters as needed
-  // };
-  // var queryString = new URLSearchParams(queryParams).toString();
-  // var requestUrl = apiUrl + "?" + queryString;
-  //   // fetch request for arbeitnow API
-  //   var requestOptions = {
-  //       method: 'GET',
-  //       redirect: 'follow'
-  //     };
-      
-      // fetch("https://www.arbeitnow.com/api/job-board-api", requestOptions)
-      //   .then(response => response.json())
-      //   .then(result => console.log(result))
-      //   .catch(error => console.log('error', error));
+  var jobs = titleInputEl.value.trim();
+
+  if (jobs) {
+    getJobsLinks(jobs);
+
+    jobsContainerEl.textContent = '';
+    titleInputEl.value = '';
+  } 
+};
+
+
+var getJobsLinks = function (job) {
   
+ 
+    var query="&title_only="+job;
+    var apiUrl = "https://api.adzuna.com/v1/api/jobs/us/search/"+page+"?app_id=fe15ccd9&app_key=7d9de1620ebcf6cd476c7c843c768025"+query
+ 
+
     fetch(apiUrl)
       .then(function (response) {
-        return response.json();
-      })
-      .then(function (data) {
-        console.log(data)
-        //Loop over the data to generate a table, each table row will have a link to the repo url
-        for (var i = 0; i < data.length; i++) {
-          // Creating elements, tablerow, tabledata, and anchor
-          var createTableRow = document.createElement('tr');
-          var tableData = document.createElement('td');
-          var link = document.createElement('a');
-  
-          // Setting the text of link and the href of the link
-          link.textContent = data[i].html_url;
-          link.href = data[i].html_url;
-  
-          // Appending the link to the tabledata and then appending the tabledata to the tablerow
-          // The tablerow then gets appended to the tablebody
-          tableData.appendChild(link);
-          createTableRow.appendChild(tableData);
-          tableBody.appendChild(createTableRow);
+        if (response.ok) {
+          console.log(response);
+          response.json().then(function (data) {
+            console.log(data.results);
+           
+            displayJobResults(data.results,job);
+          });
+        } else {
+          alert('Error: ' + response.statusText);
         }
+      })
+      .catch(function (error) {
+          
+        alert('Unable to connect to jobs list.');
       });
-  }
-salariesForm.addEventListener('submit', getArbeitNowApi);
+  };
+  
 
-// var axios = require('axios');
 
-// async function getJobListings(apiKey, location, keywords) {
-//   try {
-//     var response = await axios.get('https://api.adzuna.com/v1/api/jobs/us/search/1', {
-//       params: {
-//         app_id: apiKey,
-//         app_key: apiKey,
-//         where: location,
-//         what: keywords
-//       }
-//     });
+  var displayJobResults = function (jobsResult, searchTerm) {
+    if (jobsResult.length === 0) {
+      jobsContainerEl.textContent = 'No results found.';
+      return;
+    }
+ 
+        
+    jobsSearchTerm.textContent = searchTerm;
+  
+    for (var i = 0; i < jobsResult.length; i++) {
+      var jobByPosition = "Company: "+ jobsResult[i].company.display_name + "\nJob Title: "+ jobsResult[i].title +"\nJob Location: "+jobsResult[i].location.area+"\n" +" Salary: "+"$"+jobsResult[i].salary_max;
+  
+      var jobEl = document.createElement('div');
+      jobEl.classList = 'list-item flex-row justify-space-between align-center';
+  
+      var titleEl = document.createElement('span');
+      titleEl.textContent = jobByPosition;
+  
+    jobEl.appendChild(titleEl);
+    jobsContainerEl.appendChild(jobEl);
+   
+    
+    }
+    
+    var nextBtnEL=document.createElement("button");    
+    nextBtnEL.classList ="waves-effect waves-light btn";
+var buttonText= document.createTextNode("More");      
+nextBtnEL.appendChild(buttonText);
+jobsContainerEl.appendChild(nextBtnEL);
+ 
+nextBtnEL.addEventListener("click",function(){
+nextBtnEL.remove();
+page++;
+jobEl.remove();
+setTimeout(3000);
+getJobsLinks(jobsSearchTerm.textContent);
+   
 
-//     // Process the response data
-//     var jobListings = response.data.results;
-//     // Do something with the job listings
 
-//     return jobListings;
-//   } catch (error) {
-//     console.error('Error retrieving job listings:', error);
-//     throw error;
-//   }
-// }
+})
 
-// // Usage example
-// var apiKey = '7d9de1620ebcf6cd476c7c843c768025';
-// var location = '';
-// var keywords = '';
+ };
+ 
 
-// getJobListings(apiKey, location, keywords)
-//   .then(jobListings => {
-//     console.log('Job Listings:', jobListings);
-//   })
-//   .catch(error => {
-//     console.error('Error:', error);
-//   });
+  jobsFormEl.addEventListener('click', formSubmitHandler);
 
-//   // Loop over the job listings to generate a table
-// for (var i = 0; i < jobListings.length; i++) {
-//   // Creating elements: table row, table data, and anchor
-//   var createTableRow = document.createElement('tr');
-//   var tableData = document.createElement('td');
-//   var link = document.createElement('a');
 
-//   // Setting the text of the link and the href of the link
-//   link.textContent = jobListings[i].html_url;
-//   link.href = jobListings[i].html_url;
 
-//   // Appending the link to the table data and then appending the table data to the table row
-//   // The table row then gets appended to the table body
-//   tableData.appendChild(link);
-//   createTableRow.appendChild(tableData);
-//   tableBody.appendChild(createTableRow);
-// }
+   
